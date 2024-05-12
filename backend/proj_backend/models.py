d8d4948894de494fb3bdc2d5161210aa47955341
Ana-Maria Cicoare <anacicoare03@gmail.com>
2024-05-12 16:13:52 +0300

    Add Card

--- backend/proj_backend/models.py
+++ backend/proj_backend/models.py
@@ -2,10 +2,26 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class Card(models.Model):
+    card_number = models.CharField(max_length=16)
+    iban = models.CharField(max_length=24, unique=True)
+    expiration_date = models.DateField()
+    cvv = models.CharField(max_length=3)
+    balance = models.FloatField(default=0)
+    is_blocked = models.BooleanField(default=False)
+    type = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.card_number
+
+
 class NormalUser(models.Model):
     email = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255, default="")
+    cards = models.ManyToManyField(Card)
 
     def __str__(self):
         return self.email
@@ -15,6 +31,7 @@ class ProducerUser(models.Model):
     email = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255, default="")
+    cards = models.ManyToManyField(Card)
 
     def __str__(self):
         return self.email
@@ -24,6 +41,7 @@ class PartnerUser(models.Model):
     email = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255, default="")
+    cards = models.ManyToManyField(Card)
 
     def __str__(self):
         return self.email
